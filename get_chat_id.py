39a7381378847dacc4f63f117abc0b99bc1158c3
Dor <108623675+D0rDa4aN919@users.noreply.github.com>
2023-08-25 19:14:41 +0300

    Update get_chat_id.py

--- get_chat_id.py
+++ get_chat_id.py
@@ -1,3 +1,10 @@
+##############################################################################
+# TelegramBot_C2C_Py - get_chat_id.py - Python Script
+# Description: Fliter and extract the chat id by the chat name and the bot API token
+# Author: Dor Dahan
+# License: MIT (See details in the LICENSE file or at the end of this script)
+##############################################################################
+
 import requests
 
 # Input the chat name to use
@@ -37,3 +44,25 @@ while not end:
                 print(f"{CHAT}: {chat_ids[key]['chat_id']}, The user: {chat_ids[key]['username']},Send this message: {chat_ids[key]['text']}.")
                 checker.append(chat_ids[key]["chat_id"])
         end = True
+
+# License Information
+# This script is open-source and released under the MIT License.
+# MIT License
+# Copyright (c) 2023 Dor Dahan
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# For more details, see the LICENSE file in the root directory of this repository
+# or visit https://github.com/D0rDa4aN919/TelegramBot_C2C_Py.
